5ac1de536a9ca83e662f12c3d11597a9e1e4aec2
Peter Donker <peter@bring2mind.net>
2017-01-11 17:03:41 +0100

    Add typscript environment

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/bootstrap/index.d.ts" />
+/// <reference path="globals/chart.js/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
+/// <reference path="modules/react/index.d.ts" />
+/// <reference path="modules/react-dom/index.d.ts" />
