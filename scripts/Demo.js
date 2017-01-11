1fd4012f9c49cc4d14f3f2782e18c3a5bb250674
Peter Donker <peter@bring2mind.net>
2017-01-11 17:01:48 +0100

    Bare bones personabar module

---
+++ scripts/Demo.js
@@ -0,0 +1,8 @@
+define(['jquery'], function ($) {
+    return {
+        init: function (wrapper, util, params, callback) {
+        },
+        load: function (params, callback) {
+        }
+    };
+});
